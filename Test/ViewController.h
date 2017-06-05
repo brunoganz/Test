46c7e74ba77fa5dd8934a7f18eb69ce70f2ad750
BrunoGanz <admin@admin808s-iMac.local>
2017-06-05 04:39:40 -0700

    Create new project for CircleCI

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by admin on 6/5/17.
+//  Copyright © 2017 admin808. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by admin on 6/5/17.
+//  Copyright © 2017 admin808. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
